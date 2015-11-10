c6f2623227f09b1c5dcf1c7e8a6389fef76df7e4
Jose <Jose@MacBook-Pro-de-Carol.local>
2015-11-10 22:25:49 +0100

    first commit

---
+++ JuegoDeMemoria.playground/Contents.swift
@@ -0,0 +1,25 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var rango = 0...100
+
+for  elem in rango {
+    
+    if elem % 5 == 0 {
+        print("\(elem) Bingo!!!")
+    }
+    if elem % 2 == 0 {
+        print("\(elem) par!!!")
+    }
+    if elem % 2 != 0 {
+        print("\(elem) impar!!!")
+    }
+    
+    if elem >= 30 && elem <= 40 {
+        print("\(elem) Viva Swift!!!")
+    }
+    
+}
+
+
